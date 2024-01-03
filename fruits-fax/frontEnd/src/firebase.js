63bce55e342053609aad7e81cdd2a27c4831f2f2
Kazi Shaffan <133924179+KaziShaffan@users.noreply.github.com>
2024-01-03 12:54:47 -0500

    Edits

--- fruits-fax/frontEnd/src/firebase.js
+++ fruits-fax/frontEnd/src/firebase.js
@@ -2,15 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "XXXXXXXXX",
   authDomain: "XXXXXXXXXXXX",
-  projectId: "fruits-fax",
+  projectId: "XXXXXXXXXXXXXX",
   storageBucket: "XXXXXXXXXXXXX",
   messagingSenderId: "XXXXXXXXXXXXXXXXX",
   appId: "XXXXXXXXXXXXXXXXXXX",
